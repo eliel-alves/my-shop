b49700dcfee718eb7d920c6ad8a86f029de9683a
Eliel Alves <elielalves.dev@gmail.com>
2024-06-08 00:40:41 -0300

    feat: add homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,31 @@
+import Image from "next/image";
+import { HomeContainer, Product } from "../styles/pages/home";
+
+import shirt1 from '../assets/camisetas/1.png'
+import shirt2 from '../assets/camisetas/2.png'
+import shirt3 from '../assets/camisetas/3.png'
+import shirt4 from '../assets/camisetas/4.png'
+
 export default function Home() {
-  return <h1>Hello World!</h1>
+  return (
+    <HomeContainer>
+      <Product>
+        <Image src={shirt1} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={shirt2} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta Y</strong>
+          <span>R$ 69,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
+  )
 }
